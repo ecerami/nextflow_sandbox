e8dd7252f0aa1d8a48a248ed7567df583f982c94
Ethan Cerami <cerami@jimmy.harvard.edu>
2019-03-04 11:09:52 -0500

    Added

---
+++ clean.sh
@@ -0,0 +1,5 @@
+rm -f alignments/*.bam
+rm -f alignments/*.bai
+
+echo "All Clean"
+ls -l alignments
---
+++ pipeline_native.sh
@@ -0,0 +1,8 @@
+echo "Converting SAM to BAM"
+samtools view -b -S -o alignments/sim_reads_aligned_1.bam alignments/sim_reads_aligned_1.sam
+
+echo "Sorting BAM"
+samtools sort -o alignments/sim_reads_aligned_1.sorted.bam alignments/sim_reads_aligned_1.bam
+
+echo "Indexing BAM"
+samtools index alignments/sim_reads_aligned_1.sorted.bam
